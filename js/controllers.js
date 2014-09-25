c4115bbfa250b9439fb2e7b829f8e83902648ac1
Thiago Colares <thiago@apimenti.com.br>
2014-09-25 00:58:37 -0300

    novos jsons e EmpresaCtrl atualizados
    empresas.json e reclamacoes.json foram movidos para json/
    Agora EmpresaCtrl usa estes json
    Há uma lista que é carregada e traz algumas reclamações por padrão

--- js/controllers.js
+++ js/controllers.js
@@ -5,8 +5,12 @@
 var protocolosApp = angular.module('protocolosApp', []);
 
 protocolosApp.controller('EmpresaCtrl', function($scope, $http) {
-	$http.get('empresas.json').success( function(data) {
+	$http.get('json/empresas.json').success( function(data) {
 		$scope.empresas = data;
+	});
+
+	$http.get('json/reclamacoes.json').success( function(data) {
+		$scope.reclamacoes = data;
 	});
 
 	$scope.adicionarReclamacao = function() {
