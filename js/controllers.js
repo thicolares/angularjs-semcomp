755c7c129cbd3c4d1fffe3a5d434b4f3f6412706
Thiago Colares <thiago@apimenti.com.br>
2014-09-25 00:29:04 -0300

    criei função para submit do form

--- js/controllers.js
+++ js/controllers.js
@@ -4,9 +4,15 @@
 
 var protocolosApp = angular.module('protocolosApp', []);
 
-
 protocolosApp.controller('EmpresaCtrl', function($scope, $http) {
 	$http.get('empresas.json').success( function(data) {
 		$scope.empresas = data;
 	});
+
+	$scope.adicionarReclamacao = function() {
+		$scope.reclamacoes.push($scope.reclamacao);
+		$scope.reclamacao = {};
+	}
+
+	$scope.reclamacoes = [];
 });
